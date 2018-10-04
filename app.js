3d05ccfe6aee46c2afd3fc6d6eb3f226c8f3511a
Byrne Reese <byrne@majordojo.com>
2018-10-04 00:02:47 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+# Cover Letter App for npmjs
+
+console.log("Dear npmjs:");
+console.log("I am a hacker and a maker.");
+console.log("I am passionate about helping others, especially developers, bring cool and innovative products to the world.");
+console.log("I would love a chance to bring my skills to your team,");
+console.log("and help you solve fun problems.");
